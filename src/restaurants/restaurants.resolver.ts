20754ac058d7f6fbaff7421498d77e47730b9321
imdaegeun <cd3818@naver.com>
2021-11-27 14:32:41 +0900

    graphql practice

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,19 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  @Query(() => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+  @Mutation(() => Boolean)
+  createRestaurant(
+    @Args() createRestaurantInput: CreateRestaurantDto,
+  ): boolean {
+    console.log(createRestaurantInput);
+    return true;
+  }
+}
